c72bc64df35e3596dc567aa69f6ffb981411ae1e
Keshav Dubey <89126219+itskeshavdubey@users.noreply.github.com>
2023-04-15 15:42:40 +0530

    work update commit

--- _next/static/chunks/pages/work-9785046ae530b50c.js
+++ _next/static/chunks/pages/work-9785046ae530b50c.js
@@ -1,134 +1,168 @@
 (self.webpackChunk_N_E = self.webpackChunk_N_E || []).push([
-  [337],
-  {
-    8953: function (e, t, i) {
-      (window.__NEXT_P = window.__NEXT_P || []).push([
-        "/work",
-        function () {
-          return i(7037);
+    [337],
+    {
+        8953: function(e, t, i) {
+            (window.__NEXT_P = window.__NEXT_P || []).push([
+                "/work",
+                function() {
+                    return i(7037);
+                },
+            ]);
         },
-      ]);
-    },
-    7037: function (e, t, i) {
-      "use strict";
-      i.r(t),
-        i.d(t, {
-          default: function () {
-            return h;
-          },
-        });
-      var n = i(5893),
-        a = i(9008),
-        s = i.n(a);
-      i(7294);
-      var l = i(7098),
-        r = i(8781),
-        o = i(832),
-        p = i(9804);
-      let c = [
-        {
-            id: 1,
-            title: "Snuxplore - Complete Student Guide Website",
-            description:
-              "Led a team of 14 people and worked across the web development, design and content teams.",
-            tags: ["React", "Next.js", "Prisma", "SCSS"],
-            link: "https://snuxplore.com/",
-            type: "Design & Development",
-          },
-          {
-            id: 2,
-            title: "Surge SNIoE - Sport Fest Website",
-            description:
-              "Worked on designing a fully responsive dynamic Surge Website with wireframing and prototyping, worked on developing the frontend.",
-            tags: ["React", "Next.js", "Figma", "Adobe Photoshop"],
-            link: "https://surgesnu.in/",
-            type: "Design & Development",
-          },
-          {
-            id: 3,
-            title: "E-Summit - The Entrepreneurship Summit of SNIoE Website",
-            description: "Worked on designing UI/UX of the website.",
-            tags: ["Figma", "Next.js", "Adobe Photoshop", "React Js ", "Scss"],
-            link: "https://esummit.ecellsnu.com/",
-            type: "Design",
-          },
-          {
-            id: 4,
-            title:
-              "Partsbaba - B2B online IT Parts, Tools and Accessories store",
-            description:
-              "Worked as an Automation Intern. Developed Bulk Order Functionality, Chatbot AI, and Automated Emails. Learnt and worked on CRM.",
-            tags: ["CRM", "Zoho"],
-            link: "https://www.partsbaba.com/",
-            type: "Development",
-          },
-          {
-            id: 5,
-            title:
-              "Yonkx",
-            description:
-              "Developed Frontend and Creatives",
-            tags: ["CRM", "Zoho"],
-            link: "https://yonkx.com/",
-            type: "Development & Design",
-          },
-        ],
-        d = () =>
-          (0, n.jsxs)(o.Z, {
-            children: [
-              (0, n.jsxs)(s(), {
-                children: [
-                  (0, n.jsx)("title", { children: "Keshav Dubey - Work" }),
-                  (0, n.jsx)(
-                    "meta",
-                    { property: "og:title", content: "Keshav Dubey - Work" },
-                    "title"
-                  ),
-                  (0, n.jsx)("meta", {
-                    name: "description",
-                    content:
-                      "Keshav Dubey's Work Section, which includes all the projects.",
-                  }),
+        7037: function(e, t, i) {
+            "use strict";
+            i.r(t),
+                i.d(t, {
+                    default: function() {
+                        return h;
+                    },
+                });
+            var n = i(5893),
+                a = i(9008),
+                s = i.n(a);
+            i(7294);
+            var l = i(7098),
+                r = i(8781),
+                o = i(832),
+                p = i(9804);
+            let c = [{
+                        id: 1,
+                        title: "Snuxplore - Complete Student Guide Website",
+                        description: "Led a team of 14 people and worked across the web development, design and content teams.",
+                        tags: ["React", "Next.js", "Prisma", "SCSS"],
+                        link: "https://snuxplore.com/",
+                        type: "Design & Development",
+                    },
+                    {
+                        id: 2,
+                        title: "Frieghtemissions - Govt. Project, IIM Bangalore ",
+                        description: "It is a project given by IIM, Bangalore. It is a government project that is a dashboard based website where a user can track their carbon emissions after entering their shipment details and can download their carbon emission report.",
+                        link: "https://freightemissions.com/",
+                        type: "UI/UX Design",
+                    },
+                    {
+                        id: 3,
+                        title: "Glitch Hackathon: Where Creativity and Innovation Collides",
+                        description: "Hackathon Event Organiser & UI/UX Designer.  Led a team of 75 individuals with diverse backgrounds and skill sets, including developers, marketers, and designers.",
+                        link: "https://glitch23.live/",
+                        type: "Event Management & Design",
+                    },
+                    {
+                        id: 4,
+                        title: "Surge SNIoE - Sport Fest Website",
+                        description: "Worked on designing a fully responsive dynamic Surge Website with wireframing and prototyping, worked on developing the frontend.",
+                        tags: ["React", "Next.js", "Figma", "Adobe Photoshop"],
+                        link: "https://surgesnu.in/",
+                        type: "Design & Development",
+                    },
+                    {
+                        id: 5,
+                        title: "Bucks - Simplifies UPI payments",
+                        description: "Bucks simplifies UPI payments, eliminating transaction fees and saving you time and money and gives an event organizing team a dashboard where they can track their payments and registrations.",
+                        tags: ["React", "Next.js", "Figma", "Adobe Photoshop"],
+                        link: "https://www.figma.com/file/g9fPj66xhyeq50OmSnMTqU/Bucks?node-id=0%3A1&t=pPf3dBFWAhUBqVgV-1",
+                        type: "UI/UX Design",
+                    },
+                    {
+                        id: 6,
+                        title: "Parkezy - Slot Booking Based Parking App",
+                        description: "Parkezy is a slot booking based parking app for Shiv Nadar University. It keeps a record of all the vehicles parked inside the parking, including the in-time and out-time of the vehicles, to reduce the crowd inside the campus.",
+                        tags: ["React", "Next.js", "Figma", "Adobe Photoshop"],
+                        link: "https://www.figma.com/file/s21awr8a9SooC0Pbsqfvse/Parkezy?node-id=0%3A1&t=IeLmGOf9rqa89t1q-1",
+                        type: "Application Design, UI/UX Design",
+                    },
+                    {
+                        id: 7,
+                        title: "E-Summit - The Entrepreneurship Summit of SNIoE Website",
+                        description: "Worked on designing UI/UX of the website.",
+                        tags: ["Figma", "Next.js", "Adobe Photoshop", "React Js ", "Scss"],
+                        link: "https://esummit.ecellsnu.com/",
+                        type: "Design",
+                    },
+                    {
+                        id: 8,
+                        title: "Partsbaba - B2B online IT Parts, Tools and Accessories store",
+                        description: "Worked as an Automation Intern. Developed Bulk Order Functionality, Chatbot AI, and Automated Emails. Learnt and worked on CRM.",
+                        tags: ["CRM", "Zoho"],
+                        link: "https://www.partsbaba.com/",
+                        type: "Development",
+                    },
+                    {
+                        id: 9,
+                        title: "Unidel - Campus Wide Food Delivery App ",
+                        description: "Unidel is a food delivery app inside the campus as our campus. Order and delivery are done by the student only.",
+                        link: "https://www.figma.com/file/WOx796SxX1tZL7Rz775sjX/Unidel?node-id=0%3A1&t=HNhTW3EbLHlp1nwJ-1",
+                        type: "Application Design, UI/UX Design",
+                    },
+                    {
+                        id: 10,
+                        title: "Crooked Lines - Annual short story competition for young writers ",
+                        description: " The Department of English at the Shiv Nadar Institute of Eminence announces its inaugural annual short story competition for young writers. Entries are welcome from Indian residents between the ages of 16 and 18 on any topic of their choice,",
+                        tags: ["Design", "Figma"],
+                        link: "https://crookedlines.live/",
+                        type: "Development & Design",
+                    },
+                    {
+                        id: 11,
+                        title: "Yonkx",
+                        description: "Developed Frontend and Creatives",
+                        tags: ["CRM", "Zoho"],
+                        link: "https://yonkx.com/",
+                        type: "Development & Design",
+                    },
                 ],
-              }),
-              (0, n.jsxs)("div", {
-                className: "flex flex-col items-center",
-                children: [
-                  (0, n.jsx)(l.Z, {
-                    classesMain: "text-center text-6xl font-bold mt-20",
-                    classesSpan: "ml-4",
-                    children: "All Work",
-                  }),
-                  (0, n.jsx)("div", {
-                    className:
-                      "grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 mt-10",
-                    children: c.map((e) => {
-                      let {
-                        id: t,
-                        title: i,
-                        description: a,
-                        type: s,
-                        link: l,
-                      } = e;
-                      return (0, n.jsx)(
-                        p.Z,
-                        { title: i, description: a, type: s, link: l },
-                        t
-                      );
-                    }),
-                  }),
-                  (0, n.jsx)(r.Z, {}),
-                ],
-              }),
-            ],
-          });
-      var h = d;
+                d = () =>
+                (0, n.jsxs)(o.Z, {
+                    children: [
+                        (0, n.jsxs)(s(), {
+                            children: [
+                                (0, n.jsx)("title", { children: "Keshav Dubey - Work" }),
+                                (0, n.jsx)(
+                                    "meta", { property: "og:title", content: "Keshav Dubey - Work" },
+                                    "title"
+                                ),
+                                (0, n.jsx)("meta", {
+                                    name: "description",
+                                    content: "Keshav Dubey's Work Section, which includes all the projects.",
+                                }),
+                            ],
+                        }),
+                        (0, n.jsxs)("div", {
+                            className: "flex flex-col items-center",
+                            children: [
+                                (0, n.jsx)(l.Z, {
+                                    classesMain: "text-center text-6xl font-bold mt-20",
+                                    classesSpan: "ml-4",
+                                    children: "All Work",
+                                }),
+                                (0, n.jsx)("div", {
+                                    className: "grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 mt-10",
+                                    children: c.map((e) => {
+                                        let {
+                                            id: t,
+                                            title: i,
+                                            description: a,
+                                            type: s,
+                                            link: l,
+                                        } = e;
+                                        return (0, n.jsx)(
+                                            p.Z, { title: i, description: a, type: s, link: l },
+                                            t
+                                        );
+                                    }),
+                                }),
+                                (0, n.jsx)(r.Z, {}),
+                            ],
+                        }),
+                    ],
+                });
+            var h = d;
+        },
     },
-  },
-  function (e) {
-    e.O(0, [346, 491, 419, 774, 888, 179], function () {
-      return e((e.s = 8953));
-    }),
-      (_N_E = e.O());
-  },
+    function(e) {
+        e.O(0, [346, 491, 419, 774, 888, 179], function() {
+                return e((e.s = 8953));
+            }),
+            (_N_E = e.O());
+    },
 ]);
